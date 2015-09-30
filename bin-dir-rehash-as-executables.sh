c73dfe6757cba5d2c046310a8b2666ecc10e54fb
Andrew Stevens <andxyz.stevens@gmail.com>
2015-09-30 15:43:50 -0400

    Add a ton of old stuff that is not proprietary tech

--- bin-dir-rehash-as-executables.sh
+++ bin-dir-rehash-as-executables.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # make my ~/bin dir files executable again
 
-find "${HOME}/bin" -type f -exec chmod o+x '{}' \;
-rehash
+find "${HOME}/bin" -type f -exec chmod +x '{}' \;
+hash -r
---
+++ rrubocop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x
+set -e
+
+rubocop -l -c .rubocop.yml --force-exclusion $(git ls-files |
+ggrep -Ei '(rb,.rake,.podspec,.jbuilder,Gemfile,Rakefile,Podfile,Thorfile,Fastfile,Vagrantfile)' |
+ggrep -v _spec)
