ba8529ce27057594afa70379559cfce96a7cf717
Andrew Stevens <andxyz.stevens@gmail.com>
2015-12-27 14:33:13 -0500

    Add some useful git sub commands

--- git-tell-me-about-your-subcommands.sh
+++ git-tell-me-about-your-subcommands.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gfind /usr/local/Cellar/git/2.5.3/libexec/git-core/ -type f -print0 |
+gfind /usr/local/Cellar/git/*/libexec/git-core/ -type f -print0 |
 gxargs -0 file |
 ggrep -i 'script' |
 gcut -d: -f1 |
