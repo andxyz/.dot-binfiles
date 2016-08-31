1d89f10a2b2db9beffd8ce0e1fa8ba6bdfa57a34
Andrew Stevens <andxyz.stevens@gmail.com>
2016-08-31 14:22:27 -0400

    update to http://shellhaters.org

--- shellhatershandbook.sh
+++ shellhatershandbook.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 # just learning some shell thanks to ryan tomayko
-# http://shellhaters.heroku.com/
-# for docs see http://shellhaters.heroku.com/posix
+# http://shellhaters.org/
+# for docs see http://shellhaters.org/
 
 set -e
-/usr/local/bin/http http://shellhaters.heroku.com |
+/usr/local/bin/http http://shellhaters.org/ |
 tr -c "a-zA-Z" "\n"           |
 grep -v  '^$'                 |
 sort                          |
