5bfad327f486fe47f6be7de1146764e5fc14c1e7
Andrew Stevens <andxyz.stevens@gmail.com>
2016-08-31 13:58:44 -0400

    Still tweaking my fun command good ol `man_builtins.sh fc`
    searching ::: fc
    
    found_at ::: /Users/andxyz/code/_clone/bash/builtins/fc.def
    
    description :::
    $BUILTIN fc
    $FUNCTION fc_builtin
    $DEPENDS_ON HISTORY
    $SHORT_DOC fc [-e ename] [-lnr] [first] [last] or fc -s [pat=rep] [command]
    Display or execute commands from the history list.
    
    fc is used to list or edit and re-execute commands from the history list.
    FIRST and LAST can be numbers specifying the range, or FIRST can be a
    string, which means the most recent command beginning with that
    string.
    
    Options:
      -e ENAME	select which editor to use.  Default is FCEDIT, then EDITOR,
    		then vi
      -l 	list lines instead of editing
      -n	omit line numbers when listing
      -r	reverse the order of the lines (newest listed first)
    
    With the `fc -s [pat=rep ...] [command]' format, COMMAND is
    re-executed after the substitution OLD=NEW is performed.
    
    A useful alias to use with this is r='fc -s', so that typing `r cc'
    runs the last command beginning with `cc' and typing `r' re-executes
    the last command.
    
    Exit Status:
    Returns success or status of executed command; non-zero if an error occurs.
    $END

--- man_builtins.sh
+++ man_builtins.sh
@@ -14,19 +14,23 @@
 ### please see: http://shellhaters.org/
 ###
 
+### some odd snippets
+# grep -R -i '$FUNCTION' ~/code/_clone/bash
+# bash -x -c 'trap -l'
+
 require 'bundler'
 require 'bundler/inline'
 # require 'bundler/ui/silent'
 
-# https://github.com/bundler/bundler/blob/d23c26442a0e4caec73b5f85c21fc4738c7bc19f/lib/bundler/inline.rb
-gemfile(install: true, options: { ui: Bundler::UI::Silent.new }) do
+# https://github.com/bundler/bundler/blob/d23c26442a0e4caec73b5f85c21fc4738c7bc19f/lib/bundler/inline.rb#L31
+gemfile(true, { ui: Bundler::UI::Silent.new }) do
   source 'https://rubygems.org'
   # ruby libs
   require 'logger'
 
   # gems
-  # gem 'pry', '~> 0.10.3'
   gem 'posix-spawn', '~> 0.3.11'
+  # gem 'pry-byebug'
 end
 
 @log = Logger.new(STDOUT)
@@ -34,18 +38,21 @@ end
 # @log.level = Logger::DEBUG
 
 at_exit do
-  if $!
-    puts "I need to think of something better than at_exit"
+  if !($ERROR_INFO.nil? || $ERROR_INFO.is_a?(SystemExit) && $ERROR_INFO.success?)
+    code = $ERROR_INFO.is_a?(SystemExit) ? $ERROR_INFO.status : 1
+    puts "failure with code: #{code}"
+    # puts "I need to think of something better than at_exit"
     puts "Example Usage: man_builtins.sh ulimit"
     puts "To see a list of builtins try: man builtins"
-    exit 2
+    exit 1
   end
 end
 
 # DEBUG_SHELL='set -x;setopt printexitvalue;'
 DEBUG_SHELL=''
 
-needle = "#{$*.fetch(0)}"
+# for ARGV aka $* see http://ruby-doc.org/stdlib-2.3.1/libdoc/English/rdoc/English.html
+needle = "#{ARGV.fetch(0)}"
 list_of_reasonable_files_to_search_orig = %x{#{DEBUG_SHELL} find \$HOME/code/_clone/bash/builtins/\*.def -iname "*#{needle}*"}
 list_of_reasonable_files_to_search = list_of_reasonable_files_to_search_orig.split(/\n/)
 
@@ -65,19 +72,19 @@ output = ""
 
 output += \
 """
-searching :: #{needle}
+searching ::: #{needle}
 """
 
 results.each do |result|
     output += \
 """
-found_at :: #{result.fetch(:filename)}
+found_at ::: #{result.fetch(:filename)}
 
-description ::
+description :::
 #{result.fetch(:description)}
 """
 end
 
-@log.info(output) if results.size > 0
+puts output if results.size > 0
 
 exit 0
