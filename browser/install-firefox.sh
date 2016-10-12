3c06b23eba0556149db4fdb40cd86b16dde757e3
Andrew Stevens <andxyz.stevens@gmail.com>
2016-10-12 14:36:35 -0400

    fix the install-firefox script

--- browser/install-firefox.sh
+++ browser/install-firefox.sh
@@ -1,4 +1,5 @@
-#!/bin/usr/env bash
+#!/usr/bin/env bash
 # fuck that about button shit
 # install current firefox thanks to https://github.com/omgmog/install-all-firefox/
+
 $HOME/code/_clone/install-all-firefox/firefoxes.sh "current" "en-US" "no_prompt" "/Applications/"
