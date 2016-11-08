27586f9b241f2039952a794b0f977bcea0f12994
Andrew Stevens <andxyz.stevens@gmail.com>
2016-11-08 13:35:16 -0500

    Update ctags script

--- cctags.sh
+++ cctags.sh
@@ -3,13 +3,22 @@
 set -x
 set -e
 
+# man ctags
+# ctags-exuberant https://manned.org/ctags-exuberant/05e759a0
+
 CTAGS_GEM_HOME=`rbenv exec gem environment gemdir`
 CTAGS_RBENV_PREFIX=`rbenv prefix`
 CTAGS_RUBY_BASENAME=`basename "${CTAGS_GEM_HOME}"`
 CTAGS_RUBY_STDLIB="${CTAGS_RBENV_PREFIX}/lib/ruby/${CTAGS_RUBY_BASENAME}"
 
+gitdir="$(/usr/local/bin/git rev-parse --git-dir)"
+trap 'rm -f "$gitdir/$$.tags"' EXIT
+
+git ls-files | \
+/usr/local/bin/ctags --sort=yes -R --tag-relative --languages=+ruby,-javascript,sql --exclude=.git --exclude=log -f"$gitdir/$$.tags" "${CTAGS_RUBY_STDLIB}" $(rbenv exec bundle list --paths)
+
 if [[ $? -ne 0 ]]; then
   exit 1
 fi
 
-/usr/local/bin/ctags -R --languages=ruby --exclude=.git --exclude=log .  "${CTAGS_RUBY_STDLIB}" $(rbenv exec bundle list --paths)
+mv "$gitdir/$$.tags" "$gitdir/tags"
