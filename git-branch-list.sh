244bdbd644dce487a8d3f844ef414f7bf24efded
Andrew Stevens <andxyz.stevens@gmail.com>
2016-12-06 15:33:59 -0500

    Update my git b command

--- git-branch-list.sh
+++ git-branch-list.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 ## some credit to http://stackoverflow.com/a/2514279
+## list branches by last committed on
+## it looks something like this
+## 2016-11-24T17:57:09-05:00    17 hours ago    by Andrew Stevens    andxyz-filter-inventory-tweak
 
-for branch in $(git branch $@ | ggrep -E -v 'HEAD' | gsed -r 's/\*//g'); do
-  echo -e $(git show --format="%ci \t %cr \t by %cn" $branch | head -n 1) \\t $branch;
+for branch in $(git branch ${@} | ggrep -E -v 'HEAD' | gsed -r 's/\*//g'); do
+  echo -e $(git show --format="%cI \t %cr \t by %cn" $branch | head -n 1) \\t $branch;
 done |
 sort -r |
 column -t -s $'\t'
