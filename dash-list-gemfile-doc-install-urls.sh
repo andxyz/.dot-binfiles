7c317c579a6a773cf8fae43f4c854cb72ca351a6
Andrew Stevens <andxyz.stevens@gmail.com>
2017-01-09 18:17:11 -0500

    Dash has a neat way to install gems for specific versions
    However you can't make a docset for specific versions
    So you have to search rubygems. Which is annoying. Especially when you have
    multiple projects that use the same gems at different versions... oh deary me

---
+++ dash-list-gemfile-doc-install-urls.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+## see https://kapeli.com/dash_install
+## Prints the url to have dash install the docs for specific versions of gems in gemfile.lock
+## ex open "dash-install://repo_name=Ruby%20Docsets&entry_name=activesupport&version=4.1.11"
+## However there is currently no way to get them into a custom docset for each appname
+## https://twitter.com/kapeli/status/649852908025040896
+## But you can search them with the `rubygems:` docset
+
+bundle list | awk -F'[ ()]+' '$2 == "*" {print "open \"dash-install://repo_name=Ruby%20Docsets&entry_name=" $3 "&version=" $4 "\"";}'
