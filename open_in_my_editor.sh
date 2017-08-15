2366c8d4405b85b0f38cfdb083620b59f940cfdd
Andrew Stevens <andxyz.stevens@gmail.com>
2017-08-15 17:19:25 -0400

    add the open_in_my_editor.sh command for iterm2

---
+++ open_in_my_editor.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# old /Users/andxyz/bin/subl \1:\2
+# new /Users/andxyz/bin/open_my_editor \1 \2 "\3" "\4" "\5"
+# -![](https://cl.ly/02033T3T3B1L/Image%202017-08-15%20at%205.15.59%20PM.png)
