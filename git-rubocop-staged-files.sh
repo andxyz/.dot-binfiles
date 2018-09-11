1fbca9b08284dec61efe1c6d79cc6c27eebf15e3
Andrew Stevens <andxyz.stevens@gmail.com>
2018-09-11 08:31:45 -0400

    use bundled rubocop

--- git-rubocop-staged-files.sh
+++ git-rubocop-staged-files.sh
@@ -17,6 +17,6 @@ interesting_files = %x{ git diff --cached --diff-filter AM --name-only }.split(/
     end
   }
 puts interesting_files.join('\n')
-system("set -x; rubocop --display-cop-names --config #{rubocop_config_file} #{interesting_files.join(' ')}; set +x") unless interesting_files.empty?
+system("set -x; bundle exec rubocop --display-cop-names --config #{rubocop_config_file} #{interesting_files.join(' ')}; set +x") unless interesting_files.empty?
 
 exit $CHILD_STATUS.to_s[-1].to_i
