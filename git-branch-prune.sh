da24db82044929933826bea061103a763e7b052c
Andrew Stevens <andxyz.stevens@gmail.com>
2019-10-21 15:04:35 -0400

    Some work on this for work related headaches

--- git-branch-prune.sh
+++ git-branch-prune.sh
@@ -9,4 +9,5 @@
 # git remote prune origin
 
 # prune local branches
-git branch --merged | grep -v "\*" | grep -v master | xargs -n 1 git branch --delete
+git branch --merged | grep -v "\*" | grep -v master | xargs -I{} -n 1 echo "git branch --delete {}";
+git branch --merged | grep -v "\*" | grep -v production | xargs -I{} -n 1 echo "git branch --delete {}";
