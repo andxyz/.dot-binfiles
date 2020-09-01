57e5ecc687d9aae631d05a6833c4b9fb09a69f0c
Andrew Stevens <andxyz.stevens@gmail.com>
2020-09-01 15:57:31 -0400

    Rubocop my new files trick

---
+++ git-rubocop-against-master-latest-rubocop-linting-rules.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+rubo_files=$(git diff --diff-filter AM --name-only master | xargs -I{} ls $PWD/{} |
+ggrep -Ei '.rb|.rake|.podspec|.jbuilder|Gemfile|Rakefile|Podfile|Thorfile|Fastfile|Vagrantfile')
+
+echo ""
+echo ""
+
+cd "${HOME}"/code/rubocop_linting/
+
+set -x
+set -e
+
+bundle exec rubocop --display-cop-names --config "${HOME}"/code/rubocop_linting/.rubocop.yml --force-exclusion ${rubo_files}
