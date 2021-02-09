8137ec96241eb93e0633558e065dc47ed775b583
Andrew Stevens <andxyz.stevens@gmail.com>
2021-02-09 15:51:52 -0500

    Speed up git-branch-list by limiting to 200
    Former-commit-id: f8ad89f298828dcedbeeb2c6a5b6f8ac134d9e9d

--- git-branch-list.sh
+++ git-branch-list.sh
@@ -5,7 +5,7 @@
 ## it looks something like this
 ## 2016-11-24T17:57:09-05:00    17 hours ago    by Andrew Stevens    andxyz-filter-inventory-tweak
 
-for branch in $(git branch ${@} --sort='-committerdate' | ggrep -E -v 'HEAD' | gsed -r 's/\*//g'); do
+for branch in $(git branch ${@} --sort='-committerdate' | ghead -n200 | ggrep -E -v 'HEAD' | gsed -r 's/\*//g'); do
   echo -e $(git show --format="%cI \t %cr \t by %cn" $branch | head -n 1) \\t $branch;
 done |
 column -t -s $'\t'
