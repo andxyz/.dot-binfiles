d07b5669446bfdb5177f3383b609823e32f0edb4
Andrew Stevens <andxyz.stevens@gmail.com>
2021-03-29 13:47:49 -0400

    Add rubocop-rspec to git rubocop one trick

--- git-rubocop-against-master.sh
+++ git-rubocop-against-master.sh
@@ -9,4 +9,4 @@ ggrep -Ei '.rb|.rake|.podspec|.jbuilder|Gemfile|Rakefile|Podfile|Thorfile|Fastfi
 echo ""
 echo ""
 
-bundle exec rubocop --display-cop-names --config .rubocop.yml --force-exclusion ${rubo_files}
+bundle exec rubocop --require rubocop-rspec --display-cop-names --config ./.rubocop.yml --force-exclusion ${rubo_files}
