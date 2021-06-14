c0b2d85fdafe0df423b857410b607b8ed4d75c3e
Andrew Stevens <andxyz.stevens@gmail.com>
2021-06-14 13:11:20 -0400

    Fixup some script bugs and paths

--- git-rubocop-against-master.sh
+++ git-rubocop-against-master.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
 set -x
-set -e
 
 rubo_files=$(git diff --diff-filter AM --name-only master |
 ggrep -Ei '.rb|.rake|.podspec|.jbuilder|Gemfile|Rakefile|Podfile|Thorfile|Fastfile|Vagrantfile')
 
+
+# "Run it twice, once with rubocop enforcement, once without"
 echo ""
 echo ""
 
 bundle exec rubocop --require rubocop-rspec --display-cop-names --config ./.rubocop.yml --force-exclusion ${rubo_files}
+
+
+echo ""
+echo ""
+
+bundle exec rubocop --display-cop-names --config ./.rubocop.yml --force-exclusion ${rubo_files}
