82de7871e3b74d2a8f6a9bc13cef736bf1596df4
Andrew Stevens <andxyz.stevens@gmail.com>
2024-03-21 21:47:20 -0400

    Bump the dash-list command

--- dash-list-gemfile-doc-install-urls.sh
+++ dash-list-gemfile-doc-install-urls.sh
@@ -1,14 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/env ruby
+#
 ## see https://kapeli.com/dash_install
 ## Prints the url to have dash install the docs for specific versions of gems in gemfile.lock
 ## ex open "dash-install://repo_name=Ruby%20Docsets&entry_name=activesupport&version=4.1.11"
-## However there is currently no way to get them into a custom docset for each appname
 ## https://twitter.com/kapeli/status/649852908025040896
 ## But you can search them with the `rubygems:` docset
 
-set -e
-set -x
-
-
 bundle check || bundle install
+
+dependencies = `bundle show | grep '*' | awk '{print $2, $3}' | sed -e 's/(//' -e 's/)//'`.split("\n")
+
 bundle list | awk -F'[ ()]+' '$2 == "*" {print "open \"dash-install://repo_name=Ruby%20Docsets&entry_name=" $3 "&version=" $4 "\"";}'
+dependencies.each do |dependency|
+  (gem_name, version) = dependency.split
+  next if gem_name =~ /rails-assets/
+
+  puts "dash-install://repo_name=Ruby\%20Docsets&entry_name=#{gem_name}&version=#{version}"
+end
