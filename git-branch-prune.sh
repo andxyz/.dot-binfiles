605799fe96d8c627c5e9f78a6d5aa5b73150af40
Andrew Stevens <andxyz.stevens@gmail.com>
2024-05-01 10:35:04 -0400

    Fixup older commands

--- git-branch-prune.sh
+++ git-branch-prune.sh
@@ -9,5 +9,4 @@
 # git remote prune origin
 
 # prune local branches
-git branch --merged | grep -v "\*" | grep -v master | xargs -I{} -n 1 echo "git branch --delete {}";
-git branch --merged | grep -v "\*" | grep -v production | xargs -I{} -n 1 echo "git branch --delete {}";
+git branch --merged | grep --invert-match "\*" | egrep --invert-match --extended-regexp "main|master|production" | xargs -I{} echo "git branch --delete {}";
