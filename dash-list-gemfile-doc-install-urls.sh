608e3f5e550360b5ce4ecc3ef5e40c02fe7de071
Andrew Stevens <andxyz.stevens@gmail.com>
2025-04-08 16:20:05 -0400

    Setup ruby linting and debugging

--- dash-list-gemfile-doc-install-urls.sh
+++ dash-list-gemfile-doc-install-urls.sh
@@ -5,15 +5,20 @@
 ## ex open "dash-install://repo_name=Ruby%20Docsets&entry_name=activesupport&version=4.1.11"
 ## https://twitter.com/kapeli/status/649852908025040896
 ## But you can search them with the `rubygems:` docset
+##
+require 'open3'
 
-bundle check || bundle install
+stdout, _stderr, _status = Open3.capture3('bundle check || bundle install')
+puts stdout
 
-dependencies = `bundle show | grep '*' | awk '{print $2, $3}' | sed -e 's/(//' -e 's/)//'`.split("\n")
+stdout_dependencies, _stderr, _status = Open3.capture3("bundle show | grep '*' | awk '{print $2, $3}' | sed -e 's/(//' -e 's/)//'")
 
-bundle list | awk -F'[ ()]+' '$2 == "*" {print "open \"dash-install://repo_name=Ruby%20Docsets&entry_name=" $3 "&version=" $4 "\"";}'
-dependencies.each do |dependency|
+# Or in bash shell:
+# bundle list | awk -F'[ ()]+' '$2 == "*" {print "open \"dash-install://repo_name=Ruby%20Docsets&entry_name=" $3 "&version=" $4 "\"";}'
+
+stdout_dependencies.split("\n").each do |dependency|
   (gem_name, version) = dependency.split
   next if gem_name =~ /rails-assets/
 
-  puts "dash-install://repo_name=Ruby\%20Docsets&entry_name=#{gem_name}&version=#{version}"
+  puts "dash-install://repo_name=Ruby%20Docsets&entry_name=#{gem_name}&version=#{version}"
 end
